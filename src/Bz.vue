3dccc9655b184a72e81d16f15eb6b66dd56190e2
ly <ly@highwe.com>
2016-09-28 15:59:10 +0800

    table基础

---
+++ src/Bz.vue
@@ -0,0 +1,70 @@
+<style lang=less>
+  img {
+    width: 20px;
+  }
+</style>
+
+<template>
+  <form class="ui form">
+    <div class="inline fields">
+      <div class="eight wide field">
+        <label>Name</label>
+        <input type="text" placeholder="First Name">
+      </div>
+      <div class="eight wide field">
+        <div class="ui right floated primary button" ><i class="search icon"></i>搜索</div>
+        <div class="ui right floated button"><i class="repeat icon"></i>重置</div>
+      </div>
+    </div>
+  </form>
+  <table class="ui celled table">
+      <thead>
+        <tr><th>Header</th>
+          <th>Header</th>
+          <th>Header</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Cell</td>
+          <td>Cell</td>
+          <td>Cell</td>
+        </tr>
+        <tr>
+          <td>Cell</td>
+          <td>Cell</td>
+          <td><img src="http://cn.vuejs.org/images/logo.png"  class="ui mini rounded image"/></td>
+        </tr>
+        <tr>
+          <td>Cell</td>
+          <td>Cell</td>
+          <td>Cell</td>
+        </tr>
+      </tbody>
+      <!--
+      <tfoot> 分页 </tfoot>
+      -->
+    </table>
+</template>
+
+<script>
+  import 'bz-semantic-ui-form'
+  import 'bz-semantic-ui-table'
+  import 'bz-semantic-ui-button'
+  export default {
+    props: {
+      call_back: {
+      }
+    },
+    components: {
+    },
+    data: function () {
+      return {
+      }
+    },
+    ready () {
+    },
+    methods: {
+    }
+  }
+</script>
---
+++ src/Doc.vue
@@ -0,0 +1,43 @@
+<style>
+</style>
+<template>
+  <div>
+    <doc :name="name"
+    :desc="desc"
+    :parm_desc="parm_desc"
+    :parms="parms"
+    :code="code"
+    >
+    <bz :call_back="call_back"></bz>
+    </doc>
+  </div>
+</template>
+<script>
+  import 'bz-semantic-ui-card'
+  import 'bz-semantic-ui-grid'
+  import Bz from './Bz'
+  import Doc from 'bz-doc'
+  export default {
+    components: {
+      Bz,
+      Doc
+    },
+    data: function () {
+      return {
+        datas: [1],
+        name: 'bz-crud',
+        desc: 'CRUD',
+        parms: [
+          {parm: '', desc: 'crud操作'}
+        ],
+        parm_desc: `注意，如果使用的组件有路由，那么最好在切换路由的时候发送消息，解除绑定(参看本例子) <code>this.$broadcast('unbind-scroll')</code>`,
+        code: `<crud  :call_back="call_back"></crud>`
+      }
+    },
+    methods: {
+      call_back: function () {
+        console.log('crud')
+      }
+    }
+  }
+</script>
